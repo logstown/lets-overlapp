3aa9fa01b929956c28099e4a526199851a168324
Logan Joecks <ljoecks@gmail.com>
2025-05-11 09:34:11 -0400

    remove custom background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-base-200 antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
